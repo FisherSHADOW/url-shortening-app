9d71be13aad6f3cb9699883ff6db88d3c124d6fb
Fisher__SHADOW <hadi.h9589@gmail.com>
2023-10-26 19:00:34 +0330

    post link section fixed

--- src/components/appSection/PostLink.jsx
+++ src/components/appSection/PostLink.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@nextui-org/react";
+import { ScrollShadow } from "@nextui-org/react";
 
 const PostLink = ({ data }) => {
   const [copiedIndex, setCopiedIndex] = useState(null);
@@ -11,23 +12,25 @@ const PostLink = ({ data }) => {
     });
   };
   return (
-    <div>
+    <ScrollShadow>
       {data?.map((data, index) => (
         <div
           key={index}
           className="flex flex-row justify-center my-3 bg-cover bg-center"
         >
-          <div className="w-[70%] h-44 bg-white  rounded-xl flex flex-col justify-between  items-center md:flex-row md:h-28  ">
-            <div className=" md:w-1/2 border-b-2 md:border-none px-4 py-4 w-[80%] text-center  ">
-              <p className="line-clamp-1 text-ellipsis overflow-hidden ">
+          <div className="w-[70%] h-44 bg-white rounded-xl flex flex-col justify-between items-center lg:flex-row lg:h-28  ">
+            <div className=" lg:w-1/2 border-b-2 lg:border-none px-4 py-4 w-[80%] text-center  ">
+              <p className="line-clamp-1 text-ellipsis overflow-hidden text-left px-2 ">
                 {data.originalUrl || ""}
               </p>
             </div>
-            <div className="w-[90%] md:w-1/2 h-20 flex flex-col justify-center gap-2 items-center bg-center md:flex-row md:justify-end md:px-6 ">
-              <p className="text-primaryCyan">{data.shortenedUrl || ""}</p>
+            <div className="w-[90%]  lg:w-1/2 h-20 flex flex-col  gap-2 items-center bg-center lg:flex-row lg:justify-end lg:px-6 ">
+              <p className="text-primaryCyan text-ellipsis overflow-hidden line-clamp-1 ">
+                {data.shortenedUrl || ""}
+              </p>
 
               <Button
-                className=" w-[90%] md:w-[150px]  py-5 mb-8 md:mb-0 bg-primaryCyan rounded-lg"
+                className=" w-[90%] lg:w-[150px]  py-5 mb-8 md:mb-0 bg-primaryCyan rounded-lg"
                 onClick={() =>
                   copyToClipboard(`https://${data.shortenedUrl}`, index)
                 }
@@ -38,7 +41,7 @@ const PostLink = ({ data }) => {
           </div>
         </div>
       ))}
-    </div>
+    </ScrollShadow>
   );
 };
 
